75f456abed9938471bf018882f447a46c814a15a
Husniddin2019 <98811010+Husniddin2019@users.noreply.github.com>
2022-02-17 15:35:15 +0500

    Update homepage.dart

--- homepage.dart
+++ homepage.dart
@@ -1,54 +1,150 @@
-import 'dart:ui';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+class Homepage extends StatefulWidget {
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
+  //static final String id = "homepage";
   @override
-  _HomePageState createState() => _HomePageState();
+  _HomepageState createState() => _HomepageState();
+  const Homepage({Key? key}) : super(key: key);
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomepageState extends State<Homepage> {
+
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Business',
+      style: optionStyle,
+    ),
+    Text(
+      'School',
+      style: optionStyle,
+    ),
+    Text(
+      'Settings',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("PDP"),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
 
-    body: Container (
-
-      alignment: Alignment.topLeft,
-        //decoration: BoxDecoration(border: Border.all(color: Colors.black, width: 3), ),
-      color: Colors.green,
-      child: Row(
-       crossAxisAlignment: CrossAxisAlignment.end,
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.lightBlue,
+        unselectedItemColor: Colors.white,
 
 
-      children:  [
-          Container(
-            margin: new EdgeInsets.symmetric(vertical: 40.0),
-            color: Colors.blue,
-            width: 100,
-            height: 50,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home,size: 40,),
+            label: 'Home',
+
           ),
-        Container(
-          margin: new EdgeInsets.symmetric(vertical: 40.0),
-          color: Colors.blue,
-          width: 100,
-          height: 50,
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message,size: 40,),
+            label: 'Massege',
+
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.live_tv,size: 40,),
+            label: 'Vodeo',
+
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications,size: 40,),
+            label: 'Notifications',
+
+          ),
+
+        ],
+
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
 
 
+
+      drawer: Drawer(
+        backgroundColor: Colors.white,
+        child: Column(
+
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.blueAccent,
+              padding: const EdgeInsets.all(5),
+
+              child:  Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: 20, // Image radius
+                    child: Text("B",style: TextStyle(fontSize: 32),),
+
+                  ),
+                  Text("Baxtiyor Jumayev",style: TextStyle(color: Colors.white),),
+                  Text("b.jumayev@pdp.online",style: TextStyle(color: Colors.white),),
+
+                ],
+              )
+
+
+
+            ),
+            const ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+              selected: true,
+
+      ),
+            const ListTile(
+              leading: Icon(Icons.person_rounded),
+              title: Text('Profile'),
+
+
+            ),
+            const ListTile(
+              leading: Icon(Icons.people_rounded),
+              title: Text('Abouts'),
+
+
+            ),
+
+
+
+
+          ],
         ),
-        Container(
-          margin: new EdgeInsets.symmetric(vertical: 40.0),
 
-          color: Colors.blue,
-          width: 100, height: 50,
-
-
-        )
-      ],
-    ))
+      ),
     );
   }
 }
+
